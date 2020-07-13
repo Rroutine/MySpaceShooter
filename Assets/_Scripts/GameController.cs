820eee90015c6b2c16c8b6678c994c220baa6139
Rroutine <2937152718@qq.com>
2020-07-13 13:59:42 +0800

    提交文件

---
+++ Assets/_Scripts/GameController.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject hazard;
+    public Vector3 spawnValues;
+    private Vector3 spawnPosition = Vector3.zero;
+    private Quaternion spawnRotation;
+
+    public int hazardCount;
+    public float spawnWait;
+    public float startWait = 1.0f;
+    public float waveWait=2.0f;
+
+    public Text scoreText;
+    private int score;
+
+    public Text gameOverText;
+    private bool gameOver;
+
+    public Text restartText;
+    private bool restart;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = 0;
+        gameOverText.text = "";
+        gameOver = false;
+        restartText.text = "";
+        restart = false;
+        UpdateScore();
+        StartCoroutine(SpawnWaves());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(restart)
+        {
+            if (Input.GetKeyDown(KeyCode.R))
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+        
+    }
+
+    IEnumerator SpawnWaves()
+    {
+        yield return new WaitForSeconds(startWait);
+        while(true)
+        {
+            if (gameOver)
+            {
+                restartText.text = "按R键重新开始";
+                restart = true;
+                break;
+            }
+               
+            for (int i = 0; i < hazardCount; ++i)
+            {
+                spawnPosition.x = Random.Range(-spawnValues.x, spawnValues.x);
+                spawnPosition.z = spawnValues.z;
+                spawnRotation = Quaternion.identity;
+                Instantiate(hazard, spawnPosition, spawnRotation);
+                yield return new WaitForSeconds(spawnWait);
+            }
+            yield return new WaitForSeconds(waveWait);
+        }
+        
+           
+    }
+
+    public void AddScore(int newScoreValue)
+    {
+        score += newScoreValue;
+        UpdateScore();
+    }
+
+    void UpdateScore()
+    {
+        scoreText.text = "得分: " + score;
+    }
+
+    public void GameOver()
+    {
+        gameOver = true;
+        gameOverText.text = "游戏结束";
+    }
+}
---
+++ Assets/_Scripts/RandomRotator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomRotator : MonoBehaviour
+{
+
+    public float tumble = 10.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody>().angularVelocity = Random.insideUnitSphere * tumble;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
